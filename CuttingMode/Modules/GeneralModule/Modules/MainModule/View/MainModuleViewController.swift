d094f6a78bed8e3457c4edd18fd243dbefdb90db
artyom04 <a.s.yarmoluk@gmail.com>
2021-11-23 22:22:06 +0300

    with stackview

--- CuttingMode/Modules/GeneralModule/Modules/MainModule/View/MainModuleViewController.swift
+++ CuttingMode/Modules/GeneralModule/Modules/MainModule/View/MainModuleViewController.swift
@@ -9,42 +9,79 @@ import SnapKit
 import UIKit
 import UIModule
 
-final class MainModuleViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+final class MainModuleViewController: UIViewController {
 
-    private var collectionView: UICollectionView?
+    //    private let hintTitleLabel: UILabel = {
+    //        let label = UILabel()
+    //        label.textColor = UIColor.systemRed
+    //
+    //        return label
+    //    }()
+    //
+    //    private let hintLabel: UILabel = {
+    //        let label = UILabel()
+    //        label.textColor =  UIColor.systemBlue
+    //
+    //        return label
+    //    }()
+    //
+    //    private let stackView: UIStackView = {
+    //        let stack = UIStackView()
+    //        stack.axis = .horizontal
+    //        stack.distribution = .fillEqually
+    //
+    //        return stack
+    //    }()
+
+    var modeImage = UIImageView(image: #imageLiteral(resourceName: "145659"), highlightedImage: .none)
+
+    private var modeTitle: UILabel = {
+        let label = UILabel()
+        label.textColor = Pallete.Content.primary
+        label.text = "Фрезирование"
+        label.font =  UIFont(name: "Helvetica Neue", size: 22)
+        label.textAlignment = NSTextAlignment.center
+        return label
+    }()
+
+    var stackView = UIStackView()
+
+    func configurateStackView() {
+        view.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 40
+
+        stackView.layer.borderWidth = 3
+        if #available(iOS 13.0, *) {
+            stackView.layer.borderColor = CGColor(red: 1, green: 1, blue: 1, alpha: 1)
+        } else {
+            // Fallback on earlier versions
+        }
+
+        addElemToStackView()
+        setStackViewConstraints()
+    }
+
+    func addElemToStackView() {
 
 
-    private func setupCollectionView() {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.itemSize = CGSize(width: (view.frame.width/2)-3,
-                                 height: (view.frame.width/2)-3)
-        layout.minimumLineSpacing = 1
-        layout.minimumInteritemSpacing = 1
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        stackView.addArrangedSubview(modeImage)
+        stackView.addArrangedSubview(modeTitle)
+    }
 
-        guard let collectionView = collectionView else { return }
-        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.backgroundColor = Pallete.Backgroud.primary
-//        collectionView.snp.makeConstraints { make in
-//     
-//            make.center.equalTo(self.view)
-//        }
-        view.addSubview(collectionView)
-        collectionView.frame = view.bounds
+    func setStackViewConstraints() {
+        stackView.snp.makeConstraints { make in
+            make.width.equalTo((view.frame.width/2) - 20)
+            make.height.equalTo(view.frame.width/1.5)
+            make.top.equalToSuperview().inset(150)
+        }
     }
 
 
-    //      let label = ModeTypeView()
 
-    //    var title: UILabel = {
-    //        let button
-    //    }
 
-    let logoImage = UIImageView(image: #imageLiteral(resourceName: "145659"), contentMode: .scaleAspectFit)
-    
+
     
     //    private let mainLabel = UILabel(frame: .zero)
 
@@ -58,51 +95,55 @@ final class MainModuleViewController: UIViewController, UICollectionViewDelegate
     override func loadView() {
         super.loadView()
         //        setupMainLabel()
-        setupCollectionView()
         setupUI()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupConstraints()
-
         updateView()
         output.viewDidLoad()
     }
 
-    private func setupConstraints() {
-        view.addSubview(logoImage)
 
-        logoImage.snp.makeConstraints { make in
-            make.width.height.equalTo(50)
-            make.center.equalTo(self.view)
-        }
-        logoImage.translatesAutoresizingMaskIntoConstraints = false
-        logoImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 160).isActive = true
-        logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    }
-
-    //    private func setupMainLabel() {
-    //        mainLabel.contentMode = .scaleAspectFill
-    //    }
-    //
     private func setupUI() {
         view.backgroundColor =  Pallete.Backgroud.primary
+        view.addSubview(modeImage)
+        view.addSubview(modeTitle)
+
+//        modeImage.snp.makeConstraints { make in
+//
+////            make.left.equalToSuperview().inset(0)
+//        }
+//
+//        modeTitle.snp.makeConstraints { make in
+//
+////            make.centerX.equalTo(stackView)
+//
+//        }
+            configurateStackView()
+//                view.addSubview(stackView)
+
+          
+
+        //        view.addSubview(hintTitleLabel)
+        //        view.addSubview(hintLabel)
+
+
+        //        hintTitleLabel.snp.makeConstraints { make in
+        //            make.top.equalTo(navigationControlle).offset(20)
+        //            make.leading.trailing.equalToSuperview().inset(20)
+        //        }
+        //
+        //        hintLabel.snp.makeConstraints { make in
+        //            make.top.equalTo(hintTitleLabel.snp.bottom).offset(4)
+        //            make.leading.trailing.equalToSuperview().inset(20)
+        //        }
 
         //        view.addSubview(mainLabel)
-        ////        mainLabel.
     }
 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
-    }
 
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath)
-
-        return cell
-    }
 
 }
 
@@ -115,6 +156,12 @@ extension MainModuleViewController: MainModuleViewInput {
 
     func updateMainPhoto() {
         let params = dataSource.modelsParams()
+
+        //        hintTitleLabel.text = "hintTitleLabel"
+        //        hintLabel.text = "hintLabel"
+        //        stackView.backgroundColor = .red
+
+
         //        mainLabel.text = "12312323"
         //        mainLabel.textColor = .yellow
     }
